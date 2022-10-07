fdcdf91079b4d13b608251549dfa66f6cc93c0ab
Christoph Schneider <christoph.schneider@trihow.com>
2022-10-07 15:50:58 +0200

    Update headers

--- Samples/Intro/FB4D.DemoFmx.pas
+++ Samples/Intro/FB4D.DemoFmx.pas
@@ -1,7 +1,7 @@
 ﻿{******************************************************************************}
 {                                                                              }
 {  Delphi FB4D Library                                                         }
-{  Copyright (c) 2018-2021 Christoph Schneider                                 }
+{  Copyright (c) 2018-2022 Christoph Schneider                                 }
 {  Schneider Infosystems AG, Switzerland                                       }
 {  https://github.com/SchneiderInfosystems/FB4D                                }
 {                                                                              }
