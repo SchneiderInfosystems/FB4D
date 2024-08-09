c0430b8d436bd14d9efece6ec02816602f06fae8
SchneiderInfosystems <webmaster@schneider-infosys.ch>
2024-08-09 10:33:09 +0200

    New build batches for Version 1.6.4.606

---
+++ Samples/BuildDe12.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Build Batch for FB4D Samples with Delphi 12
+call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
+msbuild.exe "FB4D Samples.groupproj" /p:Config=Debug;Platform=Win64
+if %errorlevel% neq 0 goto Abort
+msbuild.exe "FB4D Samples VCL.groupproj" /p:Config=Debug;Platform=Win64
+pause
+exit 0
+:Abort
+echo Build failed
+pause
